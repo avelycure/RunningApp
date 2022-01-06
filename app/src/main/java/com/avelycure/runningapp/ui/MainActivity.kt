1035abe3d52b0b60715a55eed70d0a7650b5fa01
avelycure <van.emelin2012@yandex.ru>
2022-01-06 22:29:32 +0300

    Added navigation (nav_graph+bottom navigation)

--- app/src/main/java/com/avelycure/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/avelycure/runningapp/ui/MainActivity.kt
@@ -2,21 +2,30 @@ package com.avelycure.runningapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.avelycure.runningapp.R
-import com.avelycure.runningapp.data.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var runDAO: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
 
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id){
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
