adfb4618f89969b55d187c96621db22d6fabbf10
avelycure <van.emelin2012@yandex.ru>
2022-01-06 21:06:44 +0300

    Added dagger-hilt dependencies for database and dao

--- app/src/main/java/com/avelycure/runningapp/MainActivity.kt
+++ app/src/main/java/com/avelycure/runningapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.avelycure.runningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.avelycure.runningapp.data.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ app/src/main/java/com/avelycure/runningapp/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.avelycure.runningapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.avelycure.runningapp.data.RunningDatabase
+import com.avelycure.runningapp.other.Constants.RUNNING_DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRunningDatabase(
+        @ApplicationContext app: Context
+    ) = Room.databaseBuilder(
+        app,
+        RunningDatabase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Provides
+    @Singleton
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+}
