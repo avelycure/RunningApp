94324b3b2707b6cc05381e20c13a9829070e4461
avelycure <van.emelin2012@yandex.ru>
2022-01-18 23:20:17 +0300

    Saving run to db

--- app/src/main/java/com/avelycure/runningapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/avelycure/runningapp/ui/viewmodels/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.avelycure.runningapp.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.avelycure.runningapp.data.Run
 import com.avelycure.runningapp.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +13,7 @@ class MainViewModel
 @Inject constructor(
     val mainRepository: MainRepository
 ) : ViewModel() {
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
