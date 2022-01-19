9627e00e84ffaf15863481d5d98c6d22a0fbefaf
avelycure <van.emelin2012@yandex.ru>
2022-01-19 11:58:18 +0300

    Added changing name and weight in settings

--- app/src/main/java/com/avelycure/runningapp/di/AppModule.kt
+++ app/src/main/java/com/avelycure/runningapp/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.avelycure.runningapp.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.avelycure.runningapp.data.RunningDatabase
+import com.avelycure.runningapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.avelycure.runningapp.other.Constants.KEY_NAME
+import com.avelycure.runningapp.other.Constants.KEY_WEIGHT
 import com.avelycure.runningapp.other.Constants.RUNNING_DATABASE_NAME
+import com.avelycure.runningapp.other.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,4 +34,22 @@ object AppModule {
     @Provides
     @Singleton
     fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    @Provides
+    @Singleton
+    fun provideName(sharedPref: SharedPreferences) = sharedPref.getString(KEY_NAME, "") ?: ""
+
+    @Provides
+    @Singleton
+    fun provideWeight(sharedPref: SharedPreferences) = sharedPref.getFloat(KEY_WEIGHT, 80f)
+
+    @Provides
+    @Singleton
+    fun provideFirstTimeToggle(sharedPref: SharedPreferences) =
+        sharedPref.getBoolean(KEY_FIRST_TIME_TOGGLE, true)
 }
